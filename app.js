73ab9a9f97c63b2e02de03a68935df931cfa4e80
Cattla <Ted@TedMac.local>
2015-11-03 18:20:43 +0800

    debug

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var api = require('./routes/api');
 var authorize = require('./routes/authorize');
 
 var app = express();
@@ -24,8 +23,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/api', api);
 app.use('/authorize', authorize);
 app.use('*', routes);
 
