a02a4c7642c77ef2540f06273118d583fab33aaa
Cattla <cattla@me.com>
2015-11-03 23:23:02 +0800

    add revoke access link

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -4,7 +4,7 @@ var Settings = React.createClass({
 		return (
 			<div className="unauthorized">
 				<p>Because of this web application just based on <a href="https://instagram.com/developer/" target="_blank">Instagram API</a>, so you can not do any more things here. the only thing you can do is:
-					<a href="#" className="btn">Revoke access</a>
+					<a href="https://instagram.com/accounts/manage_access/" className="btn">Revoke access</a>
 				</p>
 			</div>
 		);
