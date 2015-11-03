3dca97956c058444843ef2ce1b2aca8679524ad4
Cattla <cattla@me.com>
2015-11-03 22:26:31 +0800

    +

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,17 +1,45 @@
-var React  = require('react');
-var Header  = require('./header.jsx');
+var React = require('react');
+
+var Header = React.createClass({
+	handleClick: function(e) {
+		e.preventDefault();
+		var keyword = document.getElementById('kw').value.trim();
+		if (keyword) {
+			window.location.href = '/search/' + encodeURI(keyword);
+		} else {
+			return false;
+		}
+	},
+	render: function() {
+		return (
+			<div className="topBar">
+				<header className="header">
+					<a className="home icon-home" href="/">Home</a>
+					<form>
+						<input id="kw" placeholder='Search' type="search"/>
+						<button className="icon-search" onClick={this.handleClick}></button>
+					</form>
+					<span className="logo icon-instagram"></span>
+					<span className='user'>
+						<a href={'/user/' + this.props.uid}><img src={this.props.avatar}/>{this.props.name}</a>
+					</span>
+					<a className="setting icon-cog" href="/settings"></a>
+				</header>
+			</div>
+		);
+	}
+});
 
 var Main = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <Header avatar={this.props.avatar} name={this.props.name}  uid={this.props.uid}/>
-        <div className="main" id="main">
-        </div>
-        <div id="playground"></div>
-      </div>
-    );
-  }
+	render: function() {
+		return (
+			<div>
+				<Header avatar={this.props.avatar} name={this.props.name} uid={this.props.uid}/>
+				<div className="main" id="main"></div>
+				<div id="playground"></div>
+			</div>
+		);
+	}
 });
 
 module.exports = Main;
