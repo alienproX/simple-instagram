dd0234e5cb1efed76038ecaa43d64298521f9331
Cattla <cattla@me.com>
2015-11-04 23:43:22 +0800

    remove unused function

--- src/lib/tools.jsx
+++ src/lib/tools.jsx
@@ -69,64 +69,6 @@ var tools = {
 			: '?') + 'callback=' + callbackName;
 		document.body.appendChild(script);
 	},
-	jsonpTwo: function(url, options) {
-		options = options || {};
-		var prefix = options.prefix || '__jp';
-		var param = options.param || 'callback';
-		var timeout = options.timeout
-			? options.timeout
-			: 15000;
-		var target = document.getElementsByTagName('script')[0] || document.head;
-		var script;
-		var timer;
-		var cleanup;
-		var cancel;
-		var promise;
-		var noop = function() {};
-// Generate a unique id for the request.
-		var id = prefix + (count++);
-		cleanup = function() {
-// Remove the script tag.
-			if (script && script.parentNode) {
-				script.parentNode.removeChild(script);
-			}
-			window[id] = noop;
-			if (timer) {
-				clearTimeout(timer);
-			}
-		};
-		promise = new Promise(function(resolve, reject) {
-			if (timeout) {
-				timer = setTimeout(function() {
-					cleanup();
-					reject(new Error('Timeout'));
-				}, timeout);
-			}
-			window[id] = function(data) {
-				cleanup();
-				resolve(data);
-			};
-// Add querystring component
-			url += ( ~ url.indexOf('?')
-				? '&'
-				: '?') + param + '=' + encodeURIComponent(id);
-			url = url.replace('?&', '?');
-// Create script.
-			script = document.createElement('script');
-			script.src = url;
-			target.parentNode.insertBefore(script, target);
-			cancel = function() {
-				if (window[id]) {
-					cleanup();
-					reject(new Error('Canceled'));
-				}
-			};
-		});
-		return {
-			promise: promise,
-			cancel: cancel
-		};
-	},
 	loadStart: function() {
 		document.getElementById('playground').innerHTML = '<div class="loadingWrap"><div class="loading"></div></div>';
 	},
